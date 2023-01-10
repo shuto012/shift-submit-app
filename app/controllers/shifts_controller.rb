1cd56956484f5009cb252d87d0587e76b5585ef8
shuto012 <rinGo0120X@gmail.com>
2023-01-10 10:31:03 +0900

    シフトモデル作成

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -4,8 +4,15 @@ class ShiftsController < ApplicationController
   end
 
   def new
+    @shift = Shift.new
   end
 
   def create
+    @form = Shift.new(shift_params)
+    if @shift.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 end
